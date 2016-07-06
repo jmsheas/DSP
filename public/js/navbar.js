d3e6ceff204e1c0feff8bf823f5785badb9a8859
vparthas <vprthsr2@illinois.edu>
2016-07-05 23:46:35 -0500

    Add auto hide logo on overflow

---
+++ public/js/navbar.js
@@ -0,0 +1,13 @@
+window.onresize = function (event) {
+    var logo = document.getElementById("logo-nav");
+    var leftmost = document.getElementById("first-nav");
+    var logoRect = logo.getBoundingClientRect();
+    var leftRect = leftmost.getBoundingClientRect();
+    if (logoRect.right > leftRect.left) {
+        logo.style.visibility = "hidden";
+    }
+    else {
+        logo.style.visibility = "visible";
+    }
+};
+//# sourceMappingURL=navbar.js.map
---
+++ public/js/navbar.ts
@@ -0,0 +1,16 @@
+window.onresize = function (event:Event):void
+{
+    var logo:HTMLElement = document.getElementById("logo-nav");
+    var leftmost:HTMLElement = document.getElementById("first-nav");
+    var logoRect:ClientRect = logo.getBoundingClientRect();
+    var leftRect:ClientRect = leftmost.getBoundingClientRect();
+
+    if(logoRect.right > leftRect.left)
+    {
+        logo.style.visibility = "hidden";
+    }
+    else
+    {
+        logo.style.visibility = "visible";
+    }
+}
