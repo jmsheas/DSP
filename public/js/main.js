2763fad40edc39b0202cd9d36b0d21f1542e7c9c
jmsheas <jmsheas@yahoo.com>
2016-07-14 00:40:38 -0400

    pass data

--- public/js/main.js
+++ public/js/main.js
@@ -37,56 +37,21 @@ app.controller('PageCtrl', function ($scope) {
 
 });
 
-app.controller('scholarship', function($scope) {
-
+app.controller('scholarship', function($scope, $http) {
 // fill the pdf form
-
+ $scope.form = {};
+console.log(__dirname)
   $scope.scholarship = function() {
-    var firstname = document.getElementById(firstname).value;
-    var lastname = document.getElementById(lastname).value;
-    var email = document.getElementById(email).value;
-    var phone = document.getElementById(phone).value;
-    var addressline = document.getElementById(addressline).value;
-    var city = document.getElementById(city).value;
-    var state = document.getElementById(state).value;
-    var zip = document.getElementById(zip).value;
-    var birthday = document.getElementById(birthday).value;
-    var highschool = document.getElementById(highschool).value;
-    var classrank = document.getElementById(classrank).value;
-    var gpa = document.getElementById(gpa).value;
-    var year = document.getElementById(year).value;
-    var major = document.getElementById(major).value;
-    var honors = document.getElementById(honors).value;
-    var service = document.getElementById(service).value;
-    var essay = document.getElementById(essay).value;
 
-    var sourcePDF = "files/scholarship.pdf";
-    var destinationPDF =  "files/scholarship_complete.pdf";
-    var shouldFlatten = true;
-    var data = {
-      "firstname" : firstname,
-      "lastname" : lastname,
-      "email" : email,
-      "phone" : phone,
-      "addressline" : addressline,
-      "city" : city,
-      "state" : state,
-      "zip" : zip,
-      "birthday" : birthday,
-      "highschool" : highschool,
-      "classrank" : classrank,
-      "gpa" : gpa,
-      "year" : year,
-      "major" : major,
-      "honors" : honors,
-      "service" : service,
-      "essay" : essay
-    };
-   
-    pdfFiller.fillForm( sourcePDF, destinationPDF, data, shouldFlatten, function(err) {
-      if (err) throw err;
-      console.log("In callback (we're done).");
-    });
+    console.log("called scholarship");
+    var data = $scope.form;
+    console.log(data);
+    $http({
+        method: 'POST',
+        url: '/formfill',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        data: data,
+    }).success(function () {})
   };
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 #!/bin/env node
 //  OpenShift sample Node application
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var browserify = require('browserify-middleware');
+var pdfFiller = require('pdffiller');
 
 
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server port " + server_port )
+  console.log(__dirname)
+
 });
 
 app.configure(function(){
@@ -20,3 +27,27 @@ app.configure(function(){
     app.get('/js/bundle.js', browserify(__dirname + '/public/js/main.js'));
 });
 
+
+  
+
+app.post('/public/formfill', function(req, res) {
+    console.log("called to main");
+    var data = req.body;
+    var sourcePDF = 'public/files/scholarship.pdf';
+    var destinationPDF = 'public/files/scholarshipdone.pdf';
+
+    pdfFiller.fillForm( sourcePDF, destinationPDF, data, function(err) {
+        if (err) {
+            return console.error(err);
+        }
+    });
+
+
+    //fs.unlink('public/files/scholarshipdone.pdf', function(err) {
+    //    if (err) {
+    //        return console.error(err);
+    //    }
+    //});
+
+
+});
